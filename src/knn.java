ad0a57cf01786b706717265c755d870cce80bcc7
Abhijeet Nayak <abnayak@outlook.com>
2013-11-21 00:49:22 -0500

    Bug Fixes

--- src/knn.java
+++ src/knn.java
@@ -1,38 +1,32 @@
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
-/*
-* Author: Abhijeet,Dhananjay,Kumar,Sindhu
-*/
+//Authors Abhijeet,Dhananjay,Kumar,Sindhu
 
 public class knn {
 
-	/**
-	 * @param args
-	 */
 	public static float calc5nearNeighbours(String base, ArrayList<String> old,
 			ArrayList<Float> oldAvgs, ArrayList<ArrayList<Float>> nearest5Matches) {
 
 		Float baseTempVarSum = 0f;
 		ArrayList<Float> nearest5sums = new ArrayList<Float>();
 		ArrayList<Float> nearest5nextDayVars = new ArrayList<Float>();
-
+		
 		StringTokenizer baseVals = new StringTokenizer(base);
 		int baseYearSamples = baseVals.countTokens();
 		while (baseVals.hasMoreElements()) {
 			baseTempVarSum += Float.parseFloat(baseVals.nextToken());
 		}
-
 		for (int j = 0; j < old.size(); j++) {
 			String str = old.get(j);
 			StringTokenizer oldVals = new StringTokenizer(str);
 			Float[] oldTempArr = new Float[oldVals.countTokens()];
+//			System.out.println("****" + oldVals.countTokens());
 			int k = 0;
 			while (oldVals.hasMoreElements()) {
 				oldTempArr[k] = Float.parseFloat(oldVals.nextToken());
 				k++;
 			}
-
+			// System.out.println(baseYearSamples);
 			for (int l = 0; l < oldTempArr.length - baseYearSamples; l++) {
 
 				float tempSum = 0f;
@@ -51,14 +45,17 @@ public class knn {
 					nearest5nextDayVars.add(oldTempArr[l + baseYearSamples]);
 					nearest5Matches.add(tempArr);
 
-				} else if (size <= 5) {
+				} else if (size < 5) {
 					nearest5sums.add(tempSum);
 					nearest5nextDayVars.add(oldTempArr[l + baseYearSamples]);
 					nearest5Matches.add(tempArr);
 
 					int p = nearest5sums.size();
 
-					while (p > 1 && nearest5sums.get(p - 1) < nearest5sums.get(p - 2)) {
+					while (p > 1
+							&& nearest5sums.get(p - 1) < nearest5sums
+									.get(p - 2)) {
+
 						float temp_var = nearest5sums.get(p - 1);
 						float temp_var1 = nearest5nextDayVars.get(p - 1);
 						ArrayList<Float> temp_var2 = nearest5Matches.get(p - 1);
@@ -70,16 +67,21 @@ public class knn {
 						nearest5nextDayVars.set(p - 2, temp_var1);
 						nearest5Matches.set(p - 2, temp_var2);
 						p--;
+
 					}
+
 				} else {
-					if (tempSum <= nearest5sums.get(size - 1)) {
+					if (tempSum < nearest5sums.get(size - 1)) {
 						nearest5sums.set(size - 1, tempSum);
 						nearest5nextDayVars.set(size - 1, oldTempArr[l
 								+ baseYearSamples]);
 						nearest5Matches.set(size - 1, tempArr);
 
 						int p = nearest5sums.size();
-						while (p > 1 && nearest5sums.get(p - 1) < nearest5sums.get(p - 2)) {
+						while (p > 1
+								&& nearest5sums.get(p - 1) < nearest5sums
+										.get(p - 2)) {
+
 							float temp_var = nearest5sums.get(p - 1);
 							float temp_var1 = nearest5nextDayVars.get(p - 1);
 							ArrayList<Float> temp_var2 = nearest5Matches
@@ -93,20 +95,22 @@ public class knn {
 							nearest5nextDayVars.set(p - 2, temp_var1);
 							nearest5Matches.set(p - 2, temp_var2);
 							p--;
+
 						}
 					}
 				}
+
 			}
 		}
-
+//		System.out.println(nearest5sums.size() + " REMO "
+//				+ nearest5nextDayVars.size());
 		float sum = 0f;
-
 		for (int g = 0; g < nearest5sums.size(); g++) {
 			sum += nearest5nextDayVars.get(g);
+//			System.out.println(nearest5sums.get(g) + " REMO "
+//					+ nearest5nextDayVars.get(g));
 		}
-
 		float knnVal = sum / nearest5sums.size();
 		return knnVal;
 	}
 }
-
