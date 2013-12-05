7fb8432e904bf4189c3424587c5d5050964e9661
ksadhu <kumarsnaga@gmail.com>
2013-12-05 01:27:50 -0500

    added initial functionality

---
+++ src/web/maps.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+
+  <meta charset="utf-8">
+    <!-- <title>Marker animations with <code>setTimeout()</code></title> -->
+
+<style>
+
+html, body, #map-canvas
+{
+height: 100%;
+margin: 0px;
+padding: 0px
+}
+
+#panel
+{
+position: absolute;
+top: 5px;
+left: 50%;
+      margin-left: -180px;
+      z-index: 5;
+      background-color: #fff;
+padding: 5px;
+border: 1px solid #999;
+}
+
+</style>
+</head>
+</html>
